4e01a4d9255f72a579d3ebbf4324ebc9bcbed193
Himmelsw4nderer <nbaxheinrich20030417@gmail.com>
2021-05-21 13:31:09 +0200

    renameing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 //loading environmental variables
 import dotenv from 'dotenv'
-import registerCommands from './modules/commandSystem/registerCommands';
-import registerEvents from './modules/eventSystem/registerEvents';
+import registerCommands from './res/commandSystem/registerCommands';
+import registerEvents from './res/eventSystem/registerEvents';
 dotenv.config({ path:'./src/config.env' })
 
 //creating the client
-import HugoClient from './modules/objects/HugoClient';
+import HugoClient from './res/objects/HugoClient';
 
 //logging in the client
 Promise.all([registerCommands(), registerEvents()]).then(() => {
